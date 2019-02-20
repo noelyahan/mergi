7965e883e8c784d62e0bb6028f306791fe11200e
Noel Yahan <noelyahan@gmail.com>
2019-02-20 15:03:05 +0530

    add helper function for easin
    Former-commit-id: 746b619a3afa04160dbc139a951ee03e859a0a6f

--- examples/easing/main.go
+++ examples/easing/main.go
@@ -14,19 +14,13 @@ func mapVlaues(value, start1, stop1, start2, stop2 float64) float64 {
 
 func main() {
 	cherry, _ := mergi.Import(loader.NewFileImporter("testdata/cherry-3074284_960_720.jpg"))
+	// scale down
 	cherry, _ = mergi.Resize(cherry, uint(cherry.Bounds().Max.X/4), uint(cherry.Bounds().Max.Y/4))
-	mergi.Export(loader.NewFileExporter(cherry, "examples/easing/res/ease.png"))
+
 	images := make([]image.Image, 0)
-	//for x := 0.0; x > float64(cherry.Bounds().Max.X); x += 3.5{
-	for x := float64(cherry.Bounds().Max.X); x > 0.0; x -= 2.5{
-		t := mapVlaues(float64(x), 0, float64(cherry.Bounds().Max.X-30), 0, 1)
 
-		// this is for animate y value according to time, returns 0-1 value
-		my := ease.InBounce(t)
-
-		// again re map the 0-1 value to actual value 0-image height
-		y := mapVlaues(my, 0, 1, 0, float64(cherry.Bounds().Max.Y-30))
-
+	for x := float64(cherry.Bounds().Max.X); x > 0.0; x -= 2.5 {
+		y := ease.AnimateXY(ease.InBounce, x, 0, float64(cherry.Bounds().Max.X), 0, float64(cherry.Bounds().Max.Y))
 		img, _ := mergi.Crop(cherry, image.Pt(int(x), int(y)), image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y))
 		images = append(images, img)
 	}
