cc34ecf20981ccf87d102fb61a1edf3ef9b78663
Noel Yahan <noelyahan@gmail.com>
2019-02-21 19:51:45 +0530

    simplified ease animate api, reafactor example ease
    Former-commit-id: d5fb5c1ca30f7a47362f2040087b57f34df2742f

--- ease/ease.go
+++ ease/ease.go
@@ -1,28 +1,14 @@
 package ease
 
-import "math"
+import (
+	"math"
+)
 
 type Move func(t float64) float64
 
 
-func mapVlaues(value, start1, stop1, start2, stop2 float64) float64 {
-	return start2 + (stop2-start2)*((value-start1)/(stop1-start1))
-}
-
-func AnimateXY(move Move, px, minX, maxX, minY, maxY float64) (y float64) {
-	t := mapVlaues(px, minX, maxX, 0, 1)
-	my := move(t)
-	return mapVlaues(my, 0, 1, minY, maxY)
-}
-
-func AnimateYX(move Move, py, minX, maxX, minY, maxY float64) (x float64) {
-	t := mapVlaues(py, minY, maxY, 0, 1)
-	mx := move(t)
-	return mapVlaues(mx, 0, 1, minX, maxX)
-}
-
 // this contains easing animations algorithms
-// The visualization of theses algorothms can be found on > http://easings.net/
+// The visualization of theses algorithms can be found on > http://easings.net/
 
 func InBack(t float64) float64 {
 	s := 1.70158
@@ -30,14 +16,14 @@ func InBack(t float64) float64 {
 }
 
 func InBounce(t float64) float64 {
-	return 1 - OutBounce(1-t)
+	return 1.0 - OutBounce(1.0-t)
 }
 
 func InCirc(t float64) float64 {
-	if t >= 1 {
+	if t >= 1.0 {
 		return t
 	}
-	return -1 * (math.Sqrt(1-t*t) - 1)
+	return -1.0 * (math.Sqrt(1.0-t*t) - 1.0)
 }
 
 func InCubic(t float64) float64 {
@@ -55,23 +41,23 @@ func InElastic(t float64) float64 {
 		return 1.0
 	}
 	if p == 0 {
-		p = 1 * .3
+		p = 1.0 * .3
 	}
 	if a < 1 {
 		a = 1.0
-		s = p / 4
+		s = p / 4.0
 	} else {
-		s = p / (2 * math.Pi) * math.Asin(1/a)
+		s = p / (2.0 * math.Pi) * math.Asin(1.0/a)
 	}
-	t = t - 1
-	return -(a * math.Pow(2, 10*(t)) * math.Sin((t*1-s)*(2*math.Pi)/p))
+	t = t - 1.0
+	return -(a * math.Pow(2.0, 10.0*(t)) * math.Sin((t*1.0-s)*(2.0*math.Pi)/p))
 }
 
 func InExpo(t float64) float64 {
 	if t == 0 {
-		return 1
+		return 1.0
 	}
-	return 1 * math.Pow(2, 10*(t/1-1))
+	return 1.0 * math.Pow(2.0, 10*(t/1.0-1.0))
 }
 
 func InOutBack(t float64) float64 {
@@ -79,14 +65,14 @@ func InOutBack(t float64) float64 {
 	t = t / 0.5
 	s = s * 1.525
 	if (t) < 1 {
-		return 1 / 2 * (t * t * ((s+1)*t - s))
+		return 10 / 2.0 * (t * t * ((s+1.0)*t - s))
 	}
 	t = t - 2
-	return 1 / 2 * ((t*t*(s+1)*t + s) + 2)
+	return 1.0 / 2.0 * ((t*t*(s+1.0)*t + s) + 2.0)
 }
 
 func InOutBounce(t float64) float64 {
-	if t < 1/2 {
+	if t < 1.0/2.0 {
 		return InBounce(t*2) * .5
 	}
 	return OutBounce(t*2-1)*.5 + 1*.5
@@ -95,19 +81,19 @@ func InOutBounce(t float64) float64 {
 func InOutCirc(t float64) float64 {
 	t = t / 0.5
 	if t < 1 {
-		return -1 / 2 * (math.Sqrt(1-t*t) - 1)
+		return -1.0 / 2.0 * (math.Sqrt(1-t*t) - 1)
 	}
 	t = t - 2
-	return 1 / 2 * (math.Sqrt(1-t*t) + 1)
+	return 1.0 / 2.0 * (math.Sqrt(1-t*t) + 1)
 }
 
 func InOutCubic(t float64) float64 {
-	t = t / (1 / 2)
+	t = t / 0.5
 	if t < 1 {
-		return 1 / 2 * t * t * t
+		return 1.0 / 2.0 * t * t * t
 	}
 	t = t - 2
-	return 1 / 2 * (t*t*t + 2)
+	return 1.0 / 2.0 * (t*t*t + 2)
 }
 
 func InOutElastic(t float64) float64 {
@@ -117,7 +103,7 @@ func InOutElastic(t float64) float64 {
 	if t == 0 {
 		return 0.0
 	}
-	t = t / (1 / 2)
+	t = t / (1.0 / 2.0)
 	if t == 2 {
 		return 1.0
 	}
@@ -127,15 +113,15 @@ func InOutElastic(t float64) float64 {
 	if a < 1 {
 		a = 1.0
 		// can cause a bug
-		s = p / 4
+		s = p / 4.0
 	} else {
-		s = p / (2 * math.Pi) * math.Asin(1/a)
+		s = p / (2.0 * math.Pi) * math.Asin(1.0/a)
 	}
 	t = t - 1
 	if t < 1 {
-		return -.5 * (a * math.Pow(2, 10*t) * math.Sin((t*1-s)*(2*math.Pi)/p))
+		return -.5 * (a * math.Pow(2.0, 10*t) * math.Sin((t*1-s)*(2*math.Pi)/p))
 	}
-	return a*math.Pow(2, -10*t)*math.Sin((t*1-s)*(2*math.Pi)/p)*.5 + 1
+	return a*math.Pow(2.0, -10*t)*math.Sin((t*1-s)*(2.0*math.Pi)/p)*.5 + 1
 }
 
 func InOutExpo(t float64) float64 {
@@ -145,43 +131,43 @@ func InOutExpo(t float64) float64 {
 	if t == 1 {
 		return 1.0
 	}
-	t = t / (1 / 2)
+	t = t / (1.0 / 2.0)
 	if t < 1 {
-		return 1 / 2 * math.Pow(2, 10*(t-1))
+		return 1.0 / 2.0 * math.Pow(2.0, 10*(t-1))
 	}
 	t = t - 1
-	return 1 / 2 * (-math.Pow(2, -10*t) + 2)
+	return 1.0 / 2.0 * (-math.Pow(2.0, -10*t) + 2)
 }
 
 func InOutQuad(t float64) float64 {
-	t = t / (1 / 2)
+	t = t / (1.0 / 2.0)
 	if t < 1 {
-		return 1 / 2 * t * t
+		return 1.0 / 2.0 * t * t
 	}
 	t = t - 1
-	return -1 / 2 * (t*(t-2) - 1)
+	return -1.0 / 2.0 * (t*(t-2) - 1)
 }
 
 func InOutQuart(t float64) float64 {
-	t = t / (1 / 2)
+	t = t / 0.5
 	if t < 1 {
-		return 1 / 2 * t * t * t * t
+		return 1.0 / 2.0 * t * t * t * t
 	}
 	t = t - 2
-	return -1 / 2 * (t*t*t*t - 2)
+	return -1.0 / 2.0 * (t*t*t*t - 2.0)
 }
 
 func InOutQuint(t float64) float64 {
-	t = t / (1 / 2)
+	t = t / (1.0 / 2.0)
 	if t < 1 {
-		return (1 / 2) * t * t * t * t * t
+		return (1.0 / 2.0) * t * t * t * t * t
 	}
 	t = t - 2
-	return 1 / 2 * (t*t*t*t*t + 2)
+	return 1.0 / 2.0 * (t*t*t*t*t + 2)
 }
 
 func InOutSine(t float64) float64 {
-	return -1 / 2 * (math.Cos(math.Pi*t/1) - 1)
+	return -1.0 / 2.0 * (math.Cos(math.Pi*t/1.0) - 1)
 }
 
 func InQuad(t float64) float64 {
@@ -197,37 +183,37 @@ func InQuint(t float64) float64 {
 }
 
 func InSine(t float64) float64 {
-	return -1*math.Cos(t/1*(math.Pi/2)) + 1
+	return -1.0*math.Cos(t/1.0*(math.Pi/2.0)) + 1
 }
 
 func OutBack(t float64) float64 {
 	s := 1.70158
-	t = t/1 - 1
-	return 1 * (t*t*((s+1)*t+s) + 1)
+	t = t/1.0 - 1.0
+	return 1.0 * (t*t*((s+1)*t+s) + 1)
 }
 
 func OutBounce(t float64) float64 {
-	if t < (1 / 2.75) {
-		return 1 * (7.5625 * t * t)
-	} else if t < (2 / 2.75) {
+	if t < (1.0 / 2.75) {
+		return 1.0 * (7.5625 * t * t)
+	} else if t < (2.0 / 2.75) {
 		t = t - (1.5 / 2.75)
-		return 1 * (7.5625*t*t + .75)
+		return 1.0 * (7.5625*t*t + .75)
 	} else if t < (2.5 / 2.75) {
 		t = t - (2.25 / 2.75)
-		return 1 * (7.5625*t*t + .9375)
+		return 1.0 * (7.5625*t*t + .9375)
 	}
 	t = t - (2.625 / 2.75)
-	return 1 * (7.5625*t*t + .984375)
+	return 1.0 * (7.5625*t*t + .984375)
 }
 
 func OutCirc(t float64) float64 {
 	t = t - 1
-	return 1 * math.Sqrt(1-(t*t))
+	return 1.0 * math.Sqrt(1-(t*t))
 }
 
 func OutCubic(t float64) float64 {
 	t = t/1 - 1
-	return 1 * (t*t*t + 1)
+	return 1.0 * (t*t*t + 1)
 }
 
 func OutElastic(t float64) float64 {
@@ -245,18 +231,18 @@ func OutElastic(t float64) float64 {
 	}
 	if a < 1 {
 		a = 1.0
-		s = p / 4
+		s = p / 4.0
 	} else {
-		s = p / (2 * math.Pi) * math.Asin(1/a)
+		s = p / (2.0 * math.Pi) * math.Asin(1.0/a)
 	}
-	return a*math.Pow(2, -10*t)*math.Sin((t*1-s)*(2*math.Pi)/p) + 1
+	return a*math.Pow(2.0, -10*t)*math.Sin((t*1-s)*(2.0*math.Pi)/p) + 1
 }
 
 func OutExpo(t float64) float64 {
 	if t == 1 {
 		return 1
 	}
-	return 1 * (-math.Pow(2, -10*t/1) + 1)
+	return 1.0 * (-math.Pow(2.0, -10*t/1.0) + 1)
 }
 
 func OutQuad(t float64) float64 {
@@ -274,7 +260,7 @@ func OutQuint(t float64) float64 {
 }
 
 func OutSine(t float64) float64 {
-	return 1 * math.Sin(t/1*(math.Pi/2))
+	return 1.0 * math.Sin(t/1.0*(math.Pi/2.0))
 }
 
 func Linear(t float64) float64 {
--- examples/easing/main.go
+++ examples/easing/main.go
@@ -7,24 +7,70 @@ import (
 	"github.com/noelyahan/mergi/ease"
 )
 
-func mapVlaues(value, start1, stop1, start2, stop2 float64) float64 {
-	return start2 + (stop2-start2)*((value-start1)/(stop1-start1))
-}
-
-
 func main() {
 	cherry, _ := mergi.Import(loader.NewFileImporter("testdata/cherry-3074284_960_720.jpg"))
-	// scale down
-	cherry, _ = mergi.Resize(cherry, uint(cherry.Bounds().Max.X/4), uint(cherry.Bounds().Max.Y/4))
+	coffee, _ := mergi.Import(loader.NewFileImporter("testdata/coffee-171653_960_720.jpg"))
 
+	// scale down
+	cherrySmall, _ := mergi.Resize(cherry, uint(cherry.Bounds().Max.X/4), uint(cherry.Bounds().Max.Y/4))
+	coffeeSmall, _ := mergi.Resize(coffee, uint(coffee.Bounds().Max.X/4), uint(coffee.Bounds().Max.Y/4))
+
+	cropAnimation(cherrySmall)
+	watermarkAnimation(coffee, cherrySmall)
+	cropMergeAnimation(coffeeSmall, cherrySmall)
+}
+
+func cropAnimation(cherry image.Image) {
 	images := make([]image.Image, 0)
 
-	for x := float64(cherry.Bounds().Max.X); x > 0.0; x -= 2.5 {
-		y := ease.AnimateXY(ease.InBounce, x, 0, float64(cherry.Bounds().Max.X), 0, float64(cherry.Bounds().Max.Y))
-		img, _ := mergi.Crop(cherry, image.Pt(int(x), int(y)), image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y))
+	arr := ease.AnimatePoints(ease.OutBounce, image.Pt(0, 0), image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y), 3.5)
+	// animate with crop operation
+	for _, v := range arr {
+		img, _ := mergi.Crop(cherry, v, image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y))
 		images = append(images, img)
 	}
 
 	gif, _ := mergi.Animate(images, 2)
-	mergi.Export(loader.NewAnimationExporter(gif, "examples/easing/res/ease.gif"))
+	mergi.Export(loader.NewAnimationExporter(gif, "examples/easing/res/crop_ease.gif"))
+}
+
+func watermarkAnimation(coffee, cherry image.Image) {
+	images := make([]image.Image, 0)
+	arr := ease.AnimatePoints(ease.OutBounce, image.Pt(0, 0), image.Pt(coffee.Bounds().Max.X-cherry.Bounds().Max.X, coffee.Bounds().Max.Y-cherry.Bounds().Max.Y), 3.5)
+	// animate with watermark operation
+	for _, v := range arr {
+		img, _ := mergi.Watermark(cherry, coffee, v)
+		images = append(images, img)
+	}
+
+	gif, _ := mergi.Animate(images, 2)
+	mergi.Export(loader.NewAnimationExporter(gif, "examples/easing/res/watermark_ease.gif"))
+}
+
+func cropMergeAnimation(coffee, cherry image.Image) {
+	images := make([]image.Image, 0)
+
+	// animate with resize, crop and merge operation
+	cherryAnimArr := ease.AnimatePoints(ease.InBounce, image.Pt(0, 0), image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y), 3.5)
+	coffeeAnimArr := ease.AnimatePoints(ease.InElastic, image.Pt(0, 0), image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y), 3.5)
+
+	cherryArr := make([]image.Image, 0)
+	coffeeArr := make([]image.Image, 0)
+	for _, v := range cherryAnimArr {
+		img, _ := mergi.Crop(cherry, v, image.Pt(cherry.Bounds().Max.X, cherry.Bounds().Max.Y))
+		cherryArr = append(cherryArr, img)
+	}
+
+	for _, v := range coffeeAnimArr {
+		img, _ := mergi.Crop(coffee, v, image.Pt(coffee.Bounds().Max.X, coffee.Bounds().Max.Y))
+		coffeeArr = append(coffeeArr, img)
+	}
+
+	for i := 0; i < len(cherryAnimArr); i++ {
+		img, _ := mergi.Merge("TT", []image.Image{cherryArr[i], coffeeArr[i]})
+		images = append(images, img)
+	}
+
+	gif, _ := mergi.Animate(images, 2)
+	mergi.Export(loader.NewAnimationExporter(gif, "examples/easing/res/crop_merge_ease.gif"))
 }
