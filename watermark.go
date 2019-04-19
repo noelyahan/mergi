4b49b53f15558f7e4a57f3cf97de0f85455a7c5e
Noel Yahan <noelyahan@gmail.com>
2019-04-19 22:42:10 +0530

    add opacity function
    Former-commit-id: 992f0afb6d597147003085fcd84edf4f792c150a

--- watermark.go
+++ watermark.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"image"
 	"image/draw"
+	"image/color"
 )
 
 // Watermark uses go standard image.Image to get the watermark image and original image that want to watermark,
@@ -20,9 +21,40 @@ func Watermark(watermark, original image.Image, p image.Point) (image.Image, err
 		msg := "Mergi found a error original image"
 		return nil, errors.New(msg)
 	}
-	b := original.Bounds()
-	m := image.NewRGBA(b)
-	draw.Draw(m, b, original, image.ZP, draw.Src)
-	draw.Draw(m, watermark.Bounds().Add(p), watermark, image.ZP, draw.Over)
-	return m, nil
+	oBounds := original.Bounds()
+	oRGBA := image.NewRGBA(oBounds)
+
+	draw.Draw(oRGBA, oBounds, original, image.ZP, draw.Src)
+	draw.Draw(oRGBA, watermark.Bounds().Add(p), watermark, image.ZP, draw.Over)
+	return oRGBA, nil
+}
+
+// Opacity uses go standard image.Image to change the alpha channel of the given image,
+//
+// the floating point alpha amount has to provide with the given image and it'll return the opacity image
+//
+// for more opacity examples https://github.com/noelyahan/mergi/examples/opacity
+func Opacity(img image.Image, alpha float64) (image.Image, error) {
+	mapValues := func(value, start1, stop1, start2, stop2 float64) int {
+		return int(start2 + (stop2-start2)*((value-start1)/(stop1-start1)))
+	}
+
+	if alpha < 0 {
+		alpha = 0
+	}else if alpha > 1 {
+		alpha = 1
+	}
+
+	bounds := img.Bounds()
+	mask := image.NewAlpha(bounds)
+	for x := 0; x < bounds.Dx(); x++ {
+		for y := 0; y < bounds.Dy(); y++ {
+			r := mapValues(alpha, 1, 0, 0, 255)
+			mask.SetAlpha(x, y, color.Alpha{uint8(255 - r)})
+		}
+	}
+
+	mskWatermark := image.NewRGBA(bounds)
+	draw.DrawMask(mskWatermark, bounds, img, image.ZP, mask, image.ZP, draw.Over)
+	return mskWatermark, nil
 }
