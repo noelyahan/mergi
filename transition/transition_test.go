1767f8685d301be7e196ee0691be1185d433a7f8
Noel Yahan <noelyahan@gmail.com>
2019-04-22 23:39:12 +0530

    add split transition + test
    Former-commit-id: a5091178e602091ab2abfcc414c80bfebe7348fb

--- transition/transition_test.go
+++ transition/transition_test.go
@@ -33,3 +33,38 @@ func TestFadeOut(t *testing.T) {
 	gif, _ := mergi.Animate(res, 10)
 	mergi.Export(io.NewAnimationExporter(gif, "out.gif"))
 }
+
+func TestCover(t *testing.T) {
+	img1, img2 := getImages()
+	res := Cover(img1, img2, 10, 0.5)
+
+	gif, _ := mergi.Animate(res, 10)
+	mergi.Export(io.NewAnimationExporter(gif, "out.gif"))
+}
+
+func TestSplit(t *testing.T) {
+	img1, img2 := getImages()
+	res := Split(img1, img2, 1, 0.03)
+
+	gif, _ := mergi.Animate(res, 10)
+	mergi.Export(io.NewAnimationExporter(gif, "out.gif"))
+}
+
+func TestAll(t *testing.T) {
+	img1, img2 := getImages()
+	res1 := FadeIn(img2, img1, 1, 0.05)
+	res2 := Split(img1, img2, 1, 0.1)
+	res3 := Split(img2, img1, 1, 0.1)
+	res4 := FadeIn(img1, img2, 1, 0.05)
+
+	//res2 := Cover(img1, img2, 1, 0.05)
+	//res3 := FadeIn(img1, img2, 1, 0.05)
+	//
+	res1 = append(res1, res2...)
+	res1 = append(res1, res3...)
+	res1 = append(res1, res4...)
+
+
+	gif, _ := mergi.Animate(res1, 10)
+	mergi.Export(io.NewAnimationExporter(gif, "out.gif"))
+}
