59cf98fb6d22fe30aa2a6638f5e13b69c52b2ef8
Noel Yahan <noelyahan@gmail.com>
2019-05-03 00:24:48 +0530

    refactor code for trans api changes
    Former-commit-id: 2be013421f707903b5ad7a7919380827541004b8

--- transition.go
+++ transition.go
@@ -8,11 +8,14 @@ import (
 func Transit(imgs1, imgs2, trans []image.Image, mask color.RGBA, start, end, speed float64) []image.Image {
 	// validation imgs1, imgs2, trans must be same length || imgs1, imgs2 must be length 1
 	frames := make([]image.Image, 0)
+	// copy trans
+	t := make([]image.Image, 0)
+	t = append(t, trans...)
 	var msk image.Image
 	isSingleImage := len(imgs1) == 1 && len(imgs2) == 1
-	for i := start; i <= end; i += speed {
+	for i := start; i <=end; i += speed {
 		c := int(i)
-		msk, _ = Resize(trans[c], uint(imgs1[0].Bounds().Max.X), uint(imgs1[0].Bounds().Max.Y))
+		msk, _ = Resize(t[c], uint(imgs1[0].Bounds().Max.X), uint(imgs1[0].Bounds().Max.Y))
 		if isSingleImage {
 			c = 0
 		}
